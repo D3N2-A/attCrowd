905fc77704fc11bf521e96a829fbf0fe8c8dd850
D3N2-A <anmolsharma999937@gmail.com>
2023-03-08 19:28:13 +0530

    search bar added

---
+++ src/components/Navbar/SearchInput.tsx
@@ -0,0 +1,39 @@
+import { SearchIcon } from "@chakra-ui/icons";
+import { Flex, Input, InputGroup, InputLeftElement } from "@chakra-ui/react";
+import React from "react";
+
+type SearchInputProps = {
+  //TODO: user prop
+};
+
+const SearchInput: React.FC<SearchInputProps> = () => {
+  return (
+    <Flex margin="0 1rem" flexGrow="1">
+      <InputGroup>
+        <InputLeftElement
+          pointerEvents="none"
+          children={<SearchIcon mb={1} color="gray.500" />}
+        />
+        <Input
+          fontSize="14px"
+          bg="gray.50"
+          height="34px"
+          placeholder="Search attCrowd"
+          _placeholder={{ color: "gray.500" }}
+          _hover={{
+            bg: "white",
+            border: "1px solid",
+            borderColor: "blue.500",
+          }}
+          _focus={{
+            outline: "none",
+            border: "1px solid",
+            borderColor: "blue.500",
+          }}
+        />
+      </InputGroup>
+    </Flex>
+  );
+};
+
+export default SearchInput;
