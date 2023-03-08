feebae7f713fe97d01a38d6fcf59cfdf17fc3f8b
D3N2-A <anmolsharma999937@gmail.com>
2023-03-08 23:38:38 +0530

    Modal finalisation | start of auth inputs

--- src/components/Modal/AuthModal.tsx
+++ src/components/Modal/AuthModal.tsx
@@ -1,3 +1,4 @@
+import { useRecoilState } from "recoil";
 import {
   useDisclosure,
   Button,
@@ -7,28 +8,64 @@ import {
   ModalHeader,
   ModalCloseButton,
   ModalFooter,
+  ModalBody,
+  Flex,
 } from "@chakra-ui/react";
 import React from "react";
+import { authModalState } from "@/atom/authModalAtom";
 
 const AuthModal: React.FC = () => {
-    
-  const { isOpen, onOpen, onClose } = useDisclosure();
+  const [modalState, setModalState] = useRecoilState(authModalState);
+  const handleClose = () => {
+    setModalState((prev) => {
+      return { ...prev, open: false };
+    });
+  };
   return (
     <>
-      <Button onClick={onOpen}>Open Modal</Button>
-
-      <Modal isOpen={isOpen} onClose={onClose}>
+      <Modal isOpen={modalState.open} onClose={handleClose}>
         <ModalOverlay />
         <ModalContent>
-          <ModalHeader>Modal Title</ModalHeader>
+          <ModalHeader>
+            {modalState.view === "login"
+              ? "Log In"
+              : modalState.view === "signup"
+              ? "Sign Up"
+              : "Reset Password"}
+          </ModalHeader>
           <ModalCloseButton />
+          <ModalBody
+            display="flex"
+            alignItems="center"
+            flexDirection="column"
+            justifyContent="center"
+          >
+            <Flex
+              align="center"
+              justify="center"
+              width="70%"
+              border="1px solid red"
+            >
+              <p style={{ fontSize: "12px", fontWeight: "thin" }}>
+                By continuing, you agree are setting up a Reddit account and
+                agree to our{" "}
+                <a
+                  href="https://www.random.org/terms/2020-08-01/website"
+                  target="_blank"
+                  style={{ color: "#0079d3" }}
+                >
+                  User Agreement and Privacy Policy
+                </a>
+                .
+              </p>
 
-          <ModalFooter>
-            <Button colorScheme="blue" mr={3} onClick={onClose}>
-              Close
-            </Button>
-            <Button variant="ghost">Secondary Action</Button>
-          </ModalFooter>
+              {/* 
+              <OAuthButtons/>
+              <AuthInputs/>
+              <ResetPassword/>
+              */}
+            </Flex>
+          </ModalBody>
         </ModalContent>
       </Modal>
     </>
--- src/components/Navbar/RightContent/AuthButton.tsx
+++ src/components/Navbar/RightContent/AuthButton.tsx
@@ -1,7 +1,10 @@
 import { Button } from "@chakra-ui/react";
+import { useSetRecoilState } from "recoil";
 import React from "react";
+import { authModalState } from "@/atom/authModalAtom";
 
 const AuthButton: React.FC = () => {
+  const setModalState = useSetRecoilState(authModalState);
   return (
     <>
       <Button
@@ -10,6 +13,9 @@ const AuthButton: React.FC = () => {
         display={{ base: "none", md: "flex" }}
         width={{ base: "70px", md: "110px" }}
         mr={2}
+        onClick={() => {
+          setModalState({ open: true, view: "login" });
+        }}
       >
         Log In
       </Button>
@@ -18,6 +24,9 @@ const AuthButton: React.FC = () => {
         display={{ base: "none", md: "flex" }}
         width={{ base: "70px", md: "110px" }}
         mr={2}
+        onClick={() => {
+          setModalState({ open: true, view: "signup" });
+        }}
       >
         Sign Up
       </Button>
