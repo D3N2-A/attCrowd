f6769fe2248e16dbd0d11af69ce693950b9cf624
D3N2-A <anmolsharma999937@gmail.com>
2023-03-08 14:27:51 +0530

    layout + new start

---
+++ src/chakra/theme.ts
@@ -0,0 +1,24 @@
+import { extendTheme } from "@chakra-ui/react";
+import "@fontsource/open-sans/300.css";
+import "@fontsource/open-sans/400.css";
+import "@fontsource/open-sans/700.css";
+export const theme = extendTheme({
+  colors: {
+    brand: {
+      100: "#FE6F2B",
+    },
+  },
+  fonts: {
+    body: "Open Sans, sans-serif",
+  },
+  styles: {
+    global: () => ({
+      body: {
+        bg: "gray.200",
+      },
+    }),
+  },
+  componenets: {
+    //buttons
+  },
+});
