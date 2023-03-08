2f3a8fd1019da3b6eff6096f81324ae614bc412a
D3N2-A <anmolsharma999937@gmail.com>
2023-03-08 17:31:30 +0530

    Navbar left section svg done

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,9 +1,22 @@
+import { Flex, Image } from "@chakra-ui/react";
 import React from "react";
 
-type Props = {};
-
-const Navbar = (props: Props) => {
-  return <div>Navbar</div>;
+const Navbar: React.FC = () => {
+  return (
+    <Flex bg="white" height="48px" padding="6px 12px">
+      <Flex align="center" gap="8px" borderBottom="1px bottom #EDEFF">
+        <Image src="/images/attCrowdFace.svg" height="32px" width="32px" />
+        <Image
+          display={{ base: "none", md: "unset" }}
+          src="/images/attCrowdText.svg"
+          width="57px"
+        />
+      </Flex>
+      {/* <Directory/>
+      <SearchInput />
+      <RightContent /> */}
+    </Flex>
+  );
 };
 
 export default Navbar;
